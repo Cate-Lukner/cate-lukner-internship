09add56a215b06a8c8f8e54712910c747cefc2a8
Catherine Lukner <catluk@localhost.localdomain>
2020-06-22 17:18:55 -0500

    Creating working liveview page

---
+++ larc_website/test/larc_website_web/views/layout_view_test.exs
@@ -0,0 +1,8 @@
+defmodule LarcWebsiteWeb.LayoutViewTest do
+  use LarcWebsiteWeb.ConnCase, async: true
+
+  # When testing helpers, you may want to import Phoenix.HTML and
+  # use functions such as safe_to_string() to convert the helper
+  # result into an HTML string.
+  # import Phoenix.HTML
+end
