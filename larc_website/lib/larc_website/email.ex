608e7a837bdb227869aacbeb297997705c205070
Catherine Lukner <catluk@localhost.localdomain>
2020-07-06 15:09:17 -0500

    Navigation bar now goes from transparent to solid when scrolling. Added a footer

--- larc_website/lib/larc_website/email.ex
+++ larc_website/lib/larc_website/email.ex
@@ -1,20 +1,23 @@
 defmodule LarcWebsite.Email do
   import Bamboo.Email
 
-  use Bamboo.Phoenix,
-  view: LarcWebsiteWeb.EmailView
-
   def contact_email(params) do
+    first_name = Map.get(params, "first_name")
+    last_name = Map.get(params, "last_name")
+    contact_email_address = Map.get(params, "email_address")
+    message = Map.get(params, "message")
+
     new_email()
     |> from("catluk32@gmail.com") # Replace this with LARC email
     |> to("catluk32@gmail.com") # Replace this with LARC email
     |> subject("New Contact Message: #{Map.get(params, "subject")}")
-    |> put_header("Reply-To", Map.get(params, "email_address"))
-    |> assign(:first_name, Map.get(params, "first_name"))
-    |> assign(:last_name, Map.get(params, "last_name"))
-    |> assign(:contact_email_address, Map.get(params, "email_address"))
-    |> assign(:message, Map.get(params, "message"))
-    |> put_html_layout({LarcWebsiteWeb.LayoutView, "email.html"})
-    |> render("contact_message.html")
+    |> put_header("Reply-To", contact_email_address)
+    |> html_body("
+    <p>#{first_name} #{last_name} wrote,</p>
+    <br>
+    <p>#{message}</p>
+    <br>
+    <p>Reply-To: #{contact_email_address}</p>
+    ")
   end
 end
