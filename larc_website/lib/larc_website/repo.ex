74f75f3842ed8b1b6a0145007fea9ee6b82d48c9
Catherine Lukner <catluk@localhost.localdomain>
2020-07-06 17:10:59 -0500

    Trying to fix my mess

--- larc_website/lib/larc_website/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule LarcWebsite.Repo do
-  use Ecto.Repo,
-    otp_app: :larc_website,
-    adapter: Ecto.Adapters.MyXQL
-end
--- larc_website/lib/larc_website_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule LarcWebsiteWeb.PageController do
-  use LarcWebsiteWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
